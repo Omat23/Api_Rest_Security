4e9f2c44089d3290b74ec28dbf0021ce5a00b7ef
Omar Gonzalez <omendezcarreon@gmail.com>
2026-07-08 11:45:17 -0600

    feat(roles): agregar roles admin y user para distinguir comportamientos

---
+++ src/main/java/com/example/api_rest_security/roles/Roles.java
@@ -0,0 +1,8 @@
+package com.example.api_rest_security.roles;
+
+public enum Roles {
+
+    ADMIN, //Rol ADMIN indica un usuario con permisos superiores
+    USER //Rol USER indica un usuario sencillo
+
+}
