4015fc7e0c5d0f023435b83500993373c8df334e
Omar Gonzalez <omendezcarreon@gmail.com>
2026-07-10 09:57:34 -0600

    feat(mapper): agregar interfaz mapper con mapstruct para mapear objetos de un tipo a otro

---
+++ src/main/java/com/example/api_rest_security/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.example.api_rest_security.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper //Marcamos la interfaz como un mapper
+/*
+    Un Mapper es una clase encargada de mapear objetos de un tipo a otro, por ejemplo,
+    de un DTO a una entidad o viceversa. Comunmente se hace uso de una interfaz debido a
+    que lo unico que se requiere es definir los metodos de mapeo y no es necesario
+    implementar logica en los mismos.
+ */
+public interface UserMapper {
+
+
+
+}
