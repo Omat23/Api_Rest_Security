93768eba63c123c6518fc48ce2e2091d1ec1accb
Omar Gonzalez <omendezcarreon@gmail.com>
2026-07-10 09:45:08 -0600

    docs(service): agregar la documentacion faltante a la clase authentication_service

--- src/main/java/com/example/api_rest_security/service/AuthenticationService.java
+++ src/main/java/com/example/api_rest_security/service/AuthenticationService.java
@@ -4,10 +4,11 @@ import com.example.api_rest_security.repository.UserJpaRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-@AllArgsConstructor
+@Service //Marcamos esta clase como un servicio que podra gestionar Spring
+@AllArgsConstructor //Indicamos la generacion automatica de un constructor con todos los atributos de la clase
 public class AuthenticationService {
 
+    //Atributo requerido para guardar los usuarios en la base de datos
     private final UserJpaRepository userJpaRepository;
 
 }
