9ba66d370805b9f37046ffee6a62b91be1c9154b
Omar Gonzalez <omendezcarreon@gmail.com>
2026-07-11 02:29:07 -0600

    feat(config): agregar la coniguracion basica de spring security en el security_filter_chain

--- src/main/java/com/example/api_rest_security/config/SecurityConfig.java
+++ src/main/java/com/example/api_rest_security/config/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.example.api_rest_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // Marcamos la clase como una clase de configuracion y gestor de beans
 public class SecurityConfig {
@@ -14,4 +18,32 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean //Marcamos el metodo como un bean para ser gestionado por Spring
+    /*
+        Implementamos un metodo llamado SecurityFilterChain el cual acturara como el
+        sistema de filtros que intercepta las peticiones HTTP provenientes del cliente
+        antes de llegar a los controladores. En este metodo se pueden configurar rutas privadas
+        o publicas, rutas a las cuales se requiere un permiso especial, manejo de sesiones, etc.
+     */
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                //Indicamos las rutas publicas (libre acceso) y las que requieren de autenticacion previa para su acceso
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/auth/register").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                /*
+                    Desactivamos la proteccion CSRF debido a que estamos trabajando en una api rest
+                    la cual no manejara ni creara sesiones en un navegador web.
+                 */
+                .csrf(AbstractHttpConfigurer::disable)
+                /*
+                    Indicamos la politica de gestion de las sesiones que se creen, en este caso,
+                    STATELESS indica que no se requiere de crear sesiones en el servidor
+                 */
+                .sessionManagement(sessionManagement -> {
+                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                });
+                return httpSecurity.build();
+    }
 }
