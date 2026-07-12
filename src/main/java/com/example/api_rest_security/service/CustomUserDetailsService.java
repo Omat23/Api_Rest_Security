7222f28f0e25161028b6047a612cd2654927972c
Omar Gonzalez <omendezcarreon@gmail.com>
2026-07-11 21:19:29 -0600

    feat(service): agregar un objeto user-details-service para buscar el usuario solicitado en la base de datos

---
+++ src/main/java/com/example/api_rest_security/service/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.example.api_rest_security.service;
+
+import com.example.api_rest_security.dtos.details.CustomUserDetails;
+import com.example.api_rest_security.repository.UserJpaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service // Marcamos la clase como un servicio que gestionara Spring
+@AllArgsConstructor // Indicamos la generacion automatica de un constructor con todos los atributos
+                    // de clase
+public class CustomUserDetailsService implements UserDetailsService {
+
+    // Inyectamos un objeto de tipo UserJpaRepository para acceder a nuestra base de
+    // datos y consultar usuarios
+    private final UserJpaRepository userJpaRepository;
+
+    /**
+     * Sobreescribimos el metodo loadUserByUsername de la interfaz
+     * UserDetailsService.
+     * Este metodo se encarga de obtener la informacion de un usuario de nuestra
+     * base de datos
+     * 
+     * @param email: Indica el correo electronico del usuario que se desea obtener
+     * @return: Devuelve un objeto de tipo CustomUserDetails con la informacion del
+     *          usuario
+     * @throws UsernameNotFoundException: Si el usuario no se encuentra en la base
+     *                                    de datos
+     */
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return new CustomUserDetails(userJpaRepository.finByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email)));
+    }
+}
